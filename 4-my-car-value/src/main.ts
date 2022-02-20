c80a61d3cfbe52b52a1d5d5d09fdfb5c097afa98
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-02-20 20:35:43 +0900

    feat: creating and saving users

--- 4-my-car-value/src/main.ts
+++ 4-my-car-value/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //do not allow any additional property other than registered ones on requests.
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
