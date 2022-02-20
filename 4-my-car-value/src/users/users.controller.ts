f6fee2325c7d58734c3ed2cf590cbc89be46d130
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-02-20 22:00:07 +0900

    feat: removing records
    - removeUser(id)  DELETE

--- 4-my-car-value/src/users/users.controller.ts
+++ 4-my-car-value/src/users/users.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Param,
   Query,
+  Delete,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -27,5 +28,10 @@ export class UsersController {
   @Get()
   findAllUsers(@Query('email') email: string) {
     return this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
   }
 }
