5bdc2b3b9233909702bcc19b505cbde63158cb64
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-02-22 16:09:51 +0900

    refactor: wraping the interceptor in a Decorator

--- 4-my-car-value/src/users/users.controller.ts
+++ 4-my-car-value/src/users/users.controller.ts
@@ -15,7 +15,7 @@ import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
 
-import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
@@ -28,7 +28,8 @@ export class UsersController {
   }
 
   // @UseInterceptors(ClassSerializerInterceptor)
-  @UseInterceptors(new SerializeInterceptor(UserDto))
+  // @UseInterceptors(new SerializeInterceptor(UserDto))
+  @Serialize(UserDto)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     console.log('handler is running');
