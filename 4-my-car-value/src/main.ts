f6c950949d74c6f02a68104c48ff9b09787dcb55
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-02-22 22:16:42 +0900

    setting up Sessions

--- 4-my-car-value/src/main.ts
+++ 4-my-car-value/src/main.ts
@@ -2,8 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
+const cookieSession = require('cookike-session');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    cookieSession({
+      keys: ['asdfasdf'], //used to encrypt information inside cookeis
+    }),
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({
