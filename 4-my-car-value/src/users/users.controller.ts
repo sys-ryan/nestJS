374e4eb5829e549e6a0075581b2300fc59390196
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-02-22 22:49:59 +0900

    feat: signin and getting the current user
    - /auth/signin
    - /auth/whoami

--- 4-my-car-value/src/users/users.controller.ts
+++ 4-my-car-value/src/users/users.controller.ts
@@ -39,13 +39,22 @@ export class UsersController {
   // }
 
   @Post('/signup')
-  createUser(@Body() body: CreateUserDto) {
-    return this.authService.signup(body.email, body.password);
+  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signup(body.email, body.password);
+    session.userId = user.id;
+    return user;
   }
 
   @Post('/signin')
-  signin(@Body() body: CreateUserDto) {
-    return this.authService.signin(body.email, body.password);
+  async signin(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signin(body.email, body.password);
+    session.userId = user.id;
+    return user;
+  }
+
+  @Get('/whoami')
+  whoAmI(@Session() session: any) {
+    return this.usersService.findOne(session.userId);
   }
 
   // @UseInterceptors(ClassSerializerInterceptor)
