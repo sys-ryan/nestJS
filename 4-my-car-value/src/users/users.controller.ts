cc175271ce99f7c114ec6331e9d550bf2902b561
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-02-24 18:20:48 +0900

    connecting an interceptor to dependency injection

--- 4-my-car-value/src/users/users.controller.ts
+++ 4-my-car-value/src/users/users.controller.ts
@@ -9,6 +9,7 @@ import {
   Delete,
   NotFoundException,
   Session,
+  UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -19,8 +20,13 @@ import { UserDto } from './dtos/user.dto';
 
 import { AuthService } from './auth.service';
 
+import { CurrentUser } from './decorators/current-user.decorator';
+import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+import { User } from './user.entity';
+
 @Controller('auth')
 @Serialize(UserDto)
+@UseInterceptors(CurrentUserInterceptor)
 export class UsersController {
   constructor(
     private usersService: UsersService,
@@ -52,9 +58,14 @@ export class UsersController {
     return user;
   }
 
+  // @Get('/whoami')
+  // whoAmI(@Session() session: any) {
+  //   return this.usersService.findOne(session.userId);
+  // }
+
   @Get('/whoami')
-  whoAmI(@Session() session: any) {
-    return this.usersService.findOne(session.userId);
+  whoAmI(@CurrentUser() user: User) {
+    return user;
   }
 
   @Post('/signout')
