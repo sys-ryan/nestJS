7f9e6de244c7e741710363c866da9fc004ccae40
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-02-26 03:55:59 +0900

    setup association

--- 4-my-car-value/src/reports/reports.controller.ts
+++ 4-my-car-value/src/reports/reports.controller.ts
@@ -4,6 +4,8 @@ import { ReportsService } from './reports.service';
 
 import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../guards/auth.guard';
+import { CurrentUser } from '../users/decorators/current-user.decorator';
+import { User } from '../users/user.entity';
 
 @Controller('reports')
 export class ReportsController {
@@ -11,7 +13,7 @@ export class ReportsController {
 
   @Post()
   @UseGuards(AuthGuard)
-  createReport(@Body() body: CreateReportDto) {
-    return this.reportsService.create(body);
+  createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.create(body, user);
   }
 }
