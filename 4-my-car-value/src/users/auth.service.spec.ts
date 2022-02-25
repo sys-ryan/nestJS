8c7fc532b3a87ada0896fc93ff567811b0abc8ef
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-02-25 11:00:31 +0900

    testing the signin flow

--- 4-my-car-value/src/users/auth.service.spec.ts
+++ 4-my-car-value/src/users/auth.service.spec.ts
@@ -1,4 +1,4 @@
-import { BadRequestException } from '@nestjs/common';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { User } from './user.entity';
@@ -55,5 +55,11 @@ describe('AuthService', () => {
     await expect(
       service.signup('asdf@asdf.com', 'asdf'),
     ).rejects.toBeInstanceOf(BadRequestException);
+  });
+
+  it('throws if signin is called with an unused email', async () => {
+    await expect(
+      service.signin('asdf@asdf.com', 'asdf'),
+    ).rejects.toBeInstanceOf(NotFoundException);
   });
 });
