eb7c04834ab5b2d4a90206522d4d6460ed3fb377
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-02-26 04:03:37 +0900

    transformming properties with a DTO

--- 4-my-car-value/src/reports/reports.controller.ts
+++ 4-my-car-value/src/reports/reports.controller.ts
@@ -7,12 +7,16 @@ import { AuthGuard } from '../guards/auth.guard';
 import { CurrentUser } from '../users/decorators/current-user.decorator';
 import { User } from '../users/user.entity';
 
+import { ReportDto } from './dtos/report.dto';
+import { Serialize } from '../interceptors/serialize.interceptor';
+
 @Controller('reports')
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
 
   @Post()
   @UseGuards(AuthGuard)
+  @Serialize(ReportDto)
   createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
     return this.reportsService.create(body, user);
   }
