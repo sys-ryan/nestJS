e9cd8342921a388cad1edf1cc73b73da96b9cbd2
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-02-26 01:51:41 +0900

    test: signup

--- 4-my-car-value/test/auth.e2e-spec.ts
+++ 4-my-car-value/test/auth.e2e-spec.ts
@@ -28,4 +28,23 @@ describe('Authentication System', () => {
         expect(email).toEqual(_email);
       });
   });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const _email = 'asdf@asdf.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email: _email, password: 'asdf' })
+      .expect(201);
+
+    // cookie
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie) //set header
+      .expect(200);
+
+    expect(body.email).toEqual(_email);
+  });
 });
