797d8d56f78c7d6f379d449c68c89b2c9e4c81e3
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-02-25 10:35:37 +0900

    improve file layout

--- 4-my-car-value/src/users/auth.service.spec.ts
+++ 4-my-car-value/src/users/auth.service.spec.ts
@@ -3,29 +3,37 @@ import { AuthService } from './auth.service';
 import { User } from './user.entity';
 import { UsersService } from './users.service';
 
-it('can create an instance of auth service', async () => {
-  //Create a fake copy of thje users service
-  const fakeUsersService: Partial<UsersService> = {
-    find: () => Promise.resolve([]),
-    create: (email: string, password: string) =>
-      Promise.resolve({
-        id: 1,
-        email,
-        password,
-      } as User),
-  };
+describe('AuthService', () => {
+  let service: AuthService;
 
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      {
-        provide: UsersService,
-        useValue: fakeUsersService,
-      },
-    ],
-  }).compile();
+  beforeEach(async () => {
+    //Create a fake copy of thje users service
+    const fakeUsersService: Partial<UsersService> = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({
+          id: 1,
+          email,
+          password,
+        } as User),
+    };
 
-  const service = module.get(AuthService);
+    //Create the testing module
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
+    }).compile();
 
-  expect(service).toBeDefined();
+    // Get an instance of the service
+    service = module.get(AuthService);
+  });
+
+  it('can create an instance of auth service', async () => {
+    expect(service).toBeDefined();
+  });
 });
