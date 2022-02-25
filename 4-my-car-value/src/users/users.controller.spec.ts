6a95c740f78fae657fc9a2fdb35fd69021db56f5
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-02-25 22:07:01 +0900

    test: signin

--- 4-my-car-value/src/users/users.controller.spec.ts
+++ 4-my-car-value/src/users/users.controller.spec.ts
@@ -29,7 +29,9 @@ describe('UsersController', () => {
 
     fakeAuthService = {
       // signup: () => {},
-      // signin: () => {},
+      signin: (email: string, password: string) => {
+        return Promise.resolve({ id: 1, email, password } as User);
+      },
     };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -67,5 +69,16 @@ describe('UsersController', () => {
   it('findUser throws an error if user with given id is not found.', async () => {
     fakeUsersService.findOne = () => null;
     await expect(controller.findUser('1')).rejects.toThrow(NotFoundException);
+  });
+
+  it('signin updates session object and returns user', async () => {
+    const session = { userId: -10 };
+    const user = await controller.signin(
+      { email: 'asdf@asdf.com', password: 'asdf' },
+      session,
+    );
+
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1);
   });
 });
