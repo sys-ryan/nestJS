8decf6b6be1e6a383feb330ffff38906e8a88e9c
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-02-26 11:23:48 +0900

    add report approval

--- 4-my-car-value/src/reports/reports.controller.ts
+++ 4-my-car-value/src/reports/reports.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Patch, Param } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 
@@ -8,6 +8,7 @@ import { CurrentUser } from '../users/decorators/current-user.decorator';
 import { User } from '../users/user.entity';
 
 import { ReportDto } from './dtos/report.dto';
+import { ApproveReportDto } from './dtos/approve-report.dto';
 import { Serialize } from '../interceptors/serialize.interceptor';
 
 @Controller('reports')
@@ -20,4 +21,7 @@ export class ReportsController {
   createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
     return this.reportsService.create(body, user);
   }
+
+  @Patch('/:id')
+  approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {}
 }
