3ba80205dea99307c72810360d7ee1e1a2fb13d0
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-02-26 11:30:11 +0900

    test: report approval

--- 4-my-car-value/src/reports/reports.controller.ts
+++ 4-my-car-value/src/reports/reports.controller.ts
@@ -23,5 +23,7 @@ export class ReportsController {
   }
 
   @Patch('/:id')
-  approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {}
+  approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
+    return this.reportsService.changeApproval(id, body.approved);
+  }
 }
